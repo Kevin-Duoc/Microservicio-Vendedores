77692da0365841f3ec464f3e199233a32ef4dc72
Kevin-Duoc <k.fuenzalida@duocuc.cl>
2025-06-17 23:23:58 -0400

    v1 Vendedores
    Se documentaron los endpoint con Swagger en el Controller, tambien se corrigió la URL agregandole /api/v1

--- vendedores/src/main/java/com/perfumelandiaspa/vendedores/Controller/VendedorController.java
+++ vendedores/src/main/java/com/perfumelandiaspa/vendedores/Controller/VendedorController.java
@@ -7,9 +7,11 @@ import com.perfumelandiaspa.vendedores.Model.Vendedor;
 import com.perfumelandiaspa.vendedores.Model.Entity.VendedorEntity;
 import com.perfumelandiaspa.vendedores.Service.VendedorService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
-
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +22,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController //el controlador trabaja con un REST
-@RequestMapping("/vendedor")
+@RequestMapping("/api/v1/vendedor")
 @RequiredArgsConstructor
 public class VendedorController {
     @Autowired
     private VendedorService vendedorService;
 
+    //Documentacion con Swagger de crear vendedor
+    @Operation(
+        summary = "Crear un nuevo vendedor",
+        description = "Este EndPoint crea un nuevo vendedor con el nombre de la sucursal y la meta mensual correspondiente",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Vendedor creado exitosamente",
+                content = @Content(schema = @Schema(implementation = String.class)) //Devuelve un cuerpo tipo Json
+            ),
+            @ApiResponse(
+                responseCode = "400",
+                description = "Solicitud invalida",
+                content = @Content() //no hay cuerpo en la respuesta por ende lo vacio
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Recurso no fue encontrado",
+                content = @Content() //no hay cuerpo en la respuesta por ende lo vacio
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Error interno en el servidor",
+                content =  @Content() //no hay cuerpo en la respuesta por ende lo vacio
+            )
+        }
+    )
     @PostMapping("/crear")
     public ResponseEntity<String> crearVendedor(@RequestBody Vendedor vendedor) {
         String resultado = vendedorService.crearVendedor(vendedor);
@@ -37,7 +66,33 @@ public class VendedorController {
         }
     }
 
-
+    //Documentacion con Swagger de buscar un vendedor
+    @Operation(
+        summary = "Buscar Vendedor",
+        description = "Este EndPoint se encarga de buscar un vendedor a través de su ID",
+        responses = {
+            @ApiResponse (
+                responseCode = "200",
+                description = "Vendedor encontrado exitosamente",
+                content = @Content(schema = @Schema(implementation = VendedorEntity.class))
+            ),
+            @ApiResponse(
+                responseCode = "400",
+                description = "Solicitud invalida",
+                content = @Content()
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Recurso no fue encontrado",
+                content = @Content()
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Error interno en el servidor",
+                content = @Content()
+            )       
+        }
+    )
     @GetMapping("/buscarVendedor/{idVendedor}")
     public ResponseEntity<VendedorEntity> buscarClienteID(@PathVariable Integer idVendedor) {
         try {
@@ -48,6 +103,33 @@ public class VendedorController {
         }
     }
 
+    //Documentacion con Swagger para eliminar un Vendedor por su ID
+    @Operation(
+        summary = "Eliminar Vendedor por su ID",
+        description = "Eliminar vendedores que se encuentren en la base de datos por su ID",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Vendedor buscado y eliminado correctamente",
+                content = @Content(schema = @Schema(implementation = String.class))
+            ),
+            @ApiResponse(
+                responseCode = "400",
+                description = "Solicitud invalida",
+                content = @Content()
+            ),
+            @ApiResponse(
+                responseCode = "404",
+                description = "Recurso no fue encontrado",
+                content = @Content()
+            ),
+            @ApiResponse(
+                responseCode = "500",
+                description = "Error interno en el servidor",
+                content = @Content()
+            )
+        }
+    )
     @DeleteMapping("/eliminarVendedorPorId/{idVendedor}")
     public ResponseEntity<String> eliminarPorId(@PathVariable int idVendedor ) {
     try {
@@ -59,7 +141,5 @@ public class VendedorController {
     } catch (Exception e) {
         return ResponseEntity.internalServerError().body("Error del servidor: " + e.getMessage());
     }
-}
-
-    
+    }
 }
